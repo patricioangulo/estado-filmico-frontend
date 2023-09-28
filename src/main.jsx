550139d446e2c06bcbfbc85f45d2e433aa52f0e4
Patricio Angulo <kamefront@gmail.com>
2023-09-28 16:16:45 -0300

    test update

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,16 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className='row'>
+      <div className='col-3'>
+        <h1>Esto</h1>
+      </div>
+      <div className='col-3'>
+        <h1>Es Una</h1>
+      </div>
+      <div className='col-3'>
+        <h1>Prueba</h1>
+      </div>
+    </div>
   </React.StrictMode>,
 )
